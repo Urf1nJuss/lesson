e2e256fc4e1809ce5f8e64f7ec2c4b5ec2ba3eae
Urf1nJuss <urfinjussoo@mail.ru>
2024-05-07 22:31:53 +0300

    test

--- conftest.py
+++ conftest.py
@@ -1,23 +1,28 @@
 import pytest
 from selenium import webdriver
 
+
+# запуск в разных браузерах не реализован, не требуется по заданию
+
+# задаем параметр languege, если не передан - оставляем пустым
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='',
+                     help="Choose language")
 
 
+# если передан параметр language подставляем в ссылку
 @pytest.fixture(scope="function")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+def link(request):
+    language = request.config.getoption('language')
+    return f"https://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+
+
+# открываем браузер перед запуском теста и переходим по ссылке
+@pytest.fixture(scope="function")
+def browser(link):
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    browser.get(link)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,1 +1,11 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_avail_button(browser):
+    WebDriverWait(browser, 6).until(
+        EC.visibility_of_element_located((By.XPATH, "//button[contains(@class, 'btn-add-to-basket')]")))
+    element = browser.find_element(By.XPATH, "//button[contains(@class, 'btn-add-to-basket')]")
+    assert element     # проверяем, что кнопка есть на странице
 
